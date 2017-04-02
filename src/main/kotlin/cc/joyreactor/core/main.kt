42713a20338cd0f6bcce470f9a8ba75d6fe1a2b5
y2k <itwisterlx@gmail.com>
2017-04-02 16:37:17 +0300

    fixed: redirect erros

--- src/main/kotlin/cc/joyreactor/core/main.kt
+++ src/main/kotlin/cc/joyreactor/core/main.kt
@@ -2,13 +2,14 @@ package cc.joyreactor.core
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import java.net.URL
 
 /**
  * Created by y2k on 31/03/2017.
  **/
 
 fun main(args: Array<String>) =
-    println("Posts = ${Environment().getPosts()}")
+    println("Posts = ${Environment().getPosts("Liquid Snake")}")
 
 fun Environment.getPosts(tagId: String = ""): List<Post> =
     makeTagsPath(tagId, null)
@@ -23,8 +24,9 @@ private fun makeTagsPath(tagId: String, page: Int?): String {
 class Environment {
 
     fun downloadDocument(url: String): Document =
-        Jsoup.connect("http://joyreactor.cc/$url")
-            .userAgent("Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16")
-            .timeout(10_000)
-            .get()
+        URL("http://joyreactor.cc/$url")
+            .openConnection()
+            .apply { addRequestProperty("User-Agent", "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16") }
+            .getInputStream()
+            .use { Jsoup.parse(it, "UTF-8", "http://joyreactor.cc/$url") }
 }
