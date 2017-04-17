00f63dbe72a8c81e4025ed13291de5d449921a59
y2k <itwisterlx@gmail.com>
2017-04-17 23:45:37 +0300

    refactoring

--- src/main/kotlin/cc/joyreactor/core/domain.kt
+++ src/main/kotlin/cc/joyreactor/core/domain.kt
@@ -15,7 +15,14 @@ internal object UrlCreator {
             is TagSource -> "tag/${URLEncoder.encode(source.name, "UTF-8")}"
             is Favorite -> TODO()
         }.let { "$it/${page ?: 0}" }
+
+    fun postUrl(postId: Long) = "post/$postId"
 }
 
 internal fun getPostsWithNextPages(doc: Document) =
     Posts(parsePostsForTag(doc), parseNewPageNumber(doc))
+
+internal fun List<Comment>.filterTop(): List<Comment> =
+    filter { it.parentId == 0L && it.rating >= 0 }
+        .sortedByDescending { it.rating }
+        .take(10)
--- src/main/kotlin/cc/joyreactor/core/requests.kt
+++ src/main/kotlin/cc/joyreactor/core/requests.kt
@@ -1,10 +1,22 @@
 package cc.joyreactor.core
 
+import cc.joyreactor.core.UrlCreator.postUrl
+import cc.joyreactor.core.UrlCreator.tagsPath
+
 /**
  * Created by y2k on 17/04/2017.
  **/
 
 fun Environment.get(source: Source, page: Int? = null): Posts =
-    UrlCreator.tagsPath(source, page)
+    tagsPath(source, page)
         .let { downloadDocument(it) }
         .let(::getPostsWithNextPages)
+
+fun Environment.getDetailedPost(postId: Long): Post =
+    downloadDocument(postUrl(postId))
+        .let(::parsePost)
+        .let {
+            it.copy(
+                comments = it.comments.filterTop(),
+                attachments = it.attachments.take(3))
+        }
