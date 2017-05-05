809bf9e966d080ce83ad80bb8c1093af1d1ed1bb
y2k <itwisterlx@gmail.com>
2017-05-05 15:13:57 +0300

    add APi for limit request posts

--- src/main/kotlin/cc/joyreactor/core/domain.kt
+++ src/main/kotlin/cc/joyreactor/core/domain.kt
@@ -3,11 +3,28 @@ package cc.joyreactor.core
 import cc.joyreactor.core.Parsers.parseNewPageNumber
 import cc.joyreactor.core.Parsers.parsePostsForTag
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 import java.net.URLEncoder
 
 /**
  * Created by y2k on 16/04/2017.
  **/
+
+object JoyReactor {
+
+    fun postWithComments(element: Element, config: CommentConfig): Post {
+        val post = Parsers.post(element)
+        return when (config) {
+            is AllComments -> post
+            is TopComments ->
+                post.copy(comments = post.comments.filterTop(config.limit))
+        }
+    }
+}
+
+sealed class CommentConfig
+class AllComments : CommentConfig()
+class TopComments(val limit: Int) : CommentConfig()
 
 internal object UrlCreator {
 
@@ -24,12 +41,12 @@ internal object UrlCreator {
 internal fun getPostsWithNextPages(doc: Document) =
     Posts(parsePostsForTag(doc), parseNewPageNumber(doc))
 
-internal fun List<Comment>.filterTop(): List<Comment> =
+internal fun List<Comment>.filterTop(limit: Int): List<Comment> =
     filter { it.parentId == 0L && it.rating >= 0 }
         .sortedByDescending { it.rating }
-        .take(10)
+        .take(limit)
 
 internal fun limitComment(post: Post): Post =
     post.copy(
-        comments = post.comments.filterTop(),
+        comments = post.comments.filterTop(10),
         attachments = post.attachments.take(3))
